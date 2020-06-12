3df069ae3e62a11db58eaad4dc79aea7004c690a
rabbanisidiq <syehaj12@gmail.com>
2020-06-12 22:19:58 +0700

    'final

--- database/migrations/2020_06_10_112237_insert_default_data.php
+++ database/migrations/2020_06_10_112237_insert_default_data.php
@@ -13,12 +13,117 @@ class InsertDefaultData extends Migration
      */
     public function up()
     {
-        // Data-Data mata pelajaran RPL kelas 11
+        // Data-data jadwal
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Pemrograman Berorientasi Objek'
+            'nama_matpel' => 'Pendidikan Agama Islam'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Pemrograman Web dan Perangkat Bergerak'
+            'nama_matpel' => 'Pendidikan Pancasila dan Kewarganegaraan'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Bahasa Indonesia'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Matematika'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Sejarah'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Bahasa Inggris'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Seni Budaya'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Pendidikan Jasmani Olahraga dan Kesehatan'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Basa Sunda'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Bimbingan Konseling'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Simulasi dan Komunikasi Digital'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Fisika'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Kimia'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Kerja Bengkel dan Gambar Teknik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Dasar Listrik dan Elektronika'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Teknik Pemrogramman Mikroprosesor dan Mikrokontroler'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Gambar Teknik Listrik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Pekerjaan Dasar Elektromekanik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Sistem Komputer'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Komputer dan Jaringan Dasar'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Pemrograman Dasar'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Dasar Desain Grafis'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Mikroprosesor dan Mikrokontroler'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Penerapan Rangkaian Elektronika'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Perencanaan dan Instalasi Sistem Audio'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Penerapan Sistem Radio dan Televisi'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Intalasi Penerangan Listrik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Instalasi Motor Listrik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Instalasi Tenaga Listrik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Piranti Sensor dan Aktuator'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Sistem Kontrol Elektropneumatik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Sistem Kontrol Elektromekanik dan Elektronik'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Sistem Kontrol Terprogram'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Teknologi Jaringan Berbasis Luas(WAN)'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Admnistrasi Infrastuktur Jaringan'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Admnistrasi Sistem Jaringan'
+        ]);
+        DB::table('matpel')->insert([
+            'nama_matpel' => 'Teknologi Layanan Jaringan'
         ]);
         DB::table('matpel')->insert([
             'nama_matpel' => 'Pemodelan Perangkat Lunak'
@@ -27,194 +132,32 @@ class InsertDefaultData extends Migration
             'nama_matpel' => 'Basis Data'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Kewirausahaan dan Produk Kreatif'
+            'nama_matpel' => 'Pemrograman Berorientasi Objek'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Pendidikan Agama Islam'
+            'nama_matpel' => 'Pemrograman Web dan Perangkat Bergerak'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Bahasa Indonesia'
+            'nama_matpel' => 'Desain Grafis Percetakan'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Bahasa Sunda'
+            'nama_matpel' => 'Animasi 2D dan 3D'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Pendidikan Kewarganegaraan'
+            'nama_matpel' => 'Produk Kreatif dan Kewirausahaan'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Matematika'
+            'nama_matpel' => 'Perawatan dan Perbaikan Peralatan Audio Video'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Pendidikan Jasmani dan Kesehatan'
+            'nama_matpel' => 'Perbaikan Perawatan Listrik'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Bahasa Inggris'
+            'nama_matpel' => 'Desain Media Interaktif'
         ]);
         DB::table('matpel')->insert([
-            'nama_matpel' => 'Bimbingan Konseling'
+            'nama_matpel' => 'Teknik Pengolahan Audio Video'
         ]);
-
-        // Guru
-        DB::table('guru')->insert([
-            'nama_guru' => 'Offy Solehatun,M.Kom.',
-            'id_matpel' => 1
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Lukmanul Hakim,M.Kom.',
-            'id_matpel' => 1
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Ferry Stephanus Suwita,M.Kom.',
-            'id_matpel' => 2
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Hana Zainab Mukarromah,S.Kom.',
-            'id_matpel' => 2
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Yuyun Tresnawati,M.Kom.',
-            'id_matpel' => 3
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Ali Aljauhari,S.Kom.',
-            'id_matpel' => 4
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Rina Pujiati,S.Kom.',
-            'id_matpel' => 4
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Pak Iman',
-            'id_matpel' => 5
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Jajat Sudrajat,S.Kom.',
-            'id_matpel' => 5
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Pak Deddy',
-            'id_matpel' => 6
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Pak Jafar',
-            'id_matpel' => 7
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Pak Rizal',
-            'id_matpel' => 8
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Bu Alis',
-            'id_matpel' => 9
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Bu Susneti',
-            'id_matpel' => 10
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Pak Enjang',
-            'id_matpel' => 11
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Pak Nana',
-            'id_matpel' => 11
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Bu Dewi',
-            'id_matpel' => 12
-        ]);
-        DB::table('guru')->insert([
-            'nama_guru' => 'Bu BK',
-            'id_matpel' => 13
-        ]);
-
-        // Kelas 11 RPL
-        DB::table('kelas')->insert([
-            'nama_kelas' => 'XI RPL 1',
-            'jurusan' => 'Rekayasa Perangkat Lunak'
-        ]);
-        DB::table('kelas')->insert([
-            'nama_kelas' => 'XI RPL 2',
-            'jurusan' => 'Rekayasa Perangkat Lunak'
-        ]);
-        DB::table('kelas')->insert([
-            'nama_kelas' => 'XI RPL 3',
-            'jurusan' => 'Rekayasa Perangkat Lunak'
-        ]);
-
-        // Ruangan
-        DB::table('ruangan')->insert([
-            'gedung' => 'A',
-            'nama_ruangan' => 'A.2.1'
-        ]);
-        DB::table('ruangan')->insert([
-            'gedung' => 'A',
-            'nama_ruangan' => 'A.2.2'
-        ]);
-        DB::table('ruangan')->insert([
-            'gedung' => 'A',
-            'nama_ruangan' => 'A.2.3'
-        ]);
-        DB::table('ruangan')->insert([
-            'gedung' => 'A',
-            'nama_ruangan' => 'A.2.4'
-        ]);
-        DB::table('ruangan')->insert([
-            'gedung' => 'A',
-            'nama_ruangan' => 'A.2.5'
-        ]);
-        DB::table('ruangan')->insert([
-            'gedung' => 'A',
-            'nama_ruangan' => 'A.2.6'
-        ]);
-
-        // Jadwal XI RPL 1
-        DB::table('jadwal')->insert([
-            'hari' => 'Senin',
-            'pel_begin' => '07.15',
-            'pel_end' => '10.15',
-            'id_ruangan' => 1,
-            'id_kelas' => 1,
-            'id_matpel' => 2,
-            'id_guru' => 3
-        ]);
-        DB::table('jadwal')->insert([
-            'hari' => 'Selasa',
-            'pel_begin' => '07.15',
-            'pel_end' => '10.15',
-            'id_ruangan' => 1,
-            'id_kelas' => 2,
-            'id_matpel' => 1,
-            'id_guru' => 2
-        ]);
-        DB::table('jadwal')->insert([
-            'hari' => 'Rabu',
-            'pel_begin' => '07.15',
-            'pel_end' => '10.15',
-            'id_ruangan' => 2,
-            'id_kelas' => 3,
-            'id_matpel' => 1,
-            'id_guru' => 1
-        ]);
-        DB::table('jadwal')->insert([
-            'hari' => 'Kamis',
-            'pel_begin' => '07.15',
-            'pel_end' => '10.15',
-            'id_ruangan' => 5,
-            'id_kelas' => 2,
-            'id_matpel' => 3,
-            'id_guru' => 5
-        ]);
-        DB::table('jadwal')->insert([
-            'hari' => 'Jumat',
-            'pel_begin' => '07.15',
-            'pel_end' => '10.15',
-            'id_ruangan' => 3,
-            'id_kelas' => 1,
-            'id_matpel' => 2,
-            'id_guru' => 2
-        ]);
-
     }
 
     /**
---
+++ database/migrations/2020_06_11_114403_insert_data_guru.php
@@ -0,0 +1,476 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InsertDataGuru extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('guru')->insert([
+            'nama_guru' => 'Isnaini Marifatun,S.Sos.I',
+            'id_matpel' => 1
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nana Suryana,S.Pd.I',
+            'id_matpel' => 1
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Drs.H.Dedi Sukmayadi',
+            'id_matpel' => 1
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Mochamad Fajar Firmansyah',
+            'id_matpel' => 2
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Alis Lisnawati,S.Pd.',
+            'id_matpel' => 2
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nurulhayat,S.Pd.',
+            'id_matpel' => 3
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Septiana Sulistiyawati,S.Pd.',
+            'id_matpel' => 3
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Hj.Sri Heryani',
+            'id_matpel' => 3
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Moch. Zafar Sidiq,S.Pd.',
+            'id_matpel' => 3
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Rita Yuliati',
+            'id_matpel' => 3
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Hj.Lilis Rochyantimala,S.Sos.I',
+            'id_matpel' => 4
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Taufik Rahman,S.Pd.',
+            'id_matpel' => 4
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Drs.Ikin Sodikin',
+            'id_matpel' => 4
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Susneti Martiani,S.Pd.',
+            'id_matpel' => 4
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Wina Widiana,S.Pd.',
+            'id_matpel' => 5
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Neneng Wahdatul Fitruah,S.Pd.',
+            'id_matpel' => 6
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rd.Nurul Komala Canra,S.Pd.',
+            'id_matpel' => 6
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Fajar Dewi Ningsih,S.Pd.',
+            'id_matpel' => 6
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Neneng Wahdatul Fitriah,S.Pd.',
+            'id_matpel' => 6
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Ade Judi Julianto,S.Pd.',
+            'id_matpel' => 7
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Cucu Nursutanti,S.Pd.',
+            'id_matpel' => 7
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Drs.Enjang Sarifudin',
+            'id_matpel' => 8
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nana Suryana,M.Pd',
+            'id_matpel' => 8
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Drs.Agus Suparman',
+            'id_matpel' => 8
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Annisa Meilia,S.Pd',
+            'id_matpel' => 9
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rizal Firdaus',
+            'id_matpel' => 9
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra. Aat Atiwah',
+            'id_matpel' => 10
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Maria Hani Fitriani,S.Pd.',
+            'id_matpel' => 10
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Tinceu Rosida,S.ST.',
+            'id_matpel' => 11
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Selfa Amalia Rahman,S.Pd.',
+            'id_matpel' => 11
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Siti Mulyati,S.Pd.',
+            'id_matpel' => 12
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Ine Agustina,ST.',
+            'id_matpel' => 12
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Kustini',
+            'id_matpel' => 13
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Yayat Hidayat',
+            'id_matpel' => 13
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Agus Sugiarto S,S.Pd.',
+            'id_matpel' => 14
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Maman Somawijaya,SST.',
+            'id_matpel' => 14
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Arief Setyo Jatmiko,S.Pd.',
+            'id_matpel' => 15
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dana Bastaman,S.Pd.',
+            'id_matpel' => 15
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Hj.Laswati,SST.',
+            'id_matpel' => 15
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nurhasanah,S.Pd.',
+            'id_matpel' => 15
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Tuti Farihala,S.Pd.',
+            'id_matpel' => 15
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Yuyun Yuniarsih,S.Pd.',
+            'id_matpel' => 15
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Agi Julyawan,S.Pd.',
+            'id_matpel' => 16
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Deni Alfiansyah,S.Pd.',
+            'id_matpel' => 16
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Raden Ridwan Nurmatullah,S.',
+            'id_matpel' => 16
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Hj.Putri Elfia',
+            'id_matpel' => 17
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Triyanto,S.Pd.',
+            'id_matpel' => 18
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Yofi Fikri Islami,S.Pd.',
+            'id_matpel' => 18
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Aditya Ramadhan,S.Pd.',
+            'id_matpel' => 19
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Cuncun Cahyadi,S.Si,M.M',
+            'id_matpel' => 19
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Moh.Gilang Ramadhan,S.Pd.',
+            'id_matpel' => 20
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Tyas Sawiji,S.Pd.',
+            'id_matpel' => 20
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Jajat Drajat,ST.',
+            'id_matpel' => 21
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rani Ratnaningsih,S.Pd.',
+            'id_matpel' => 21
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Drs.Rizal Sapari',
+            'id_matpel' => 22
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Arif Syarifudin,S.T.',
+            'id_matpel' => 23
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Imas Rahayu,S.Pd.MT.',
+            'id_matpel' => 23
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Agi Julyawan,S.Pd.',
+            'id_matpel' => 24
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Deni Alfiansyah,S.Pd.',
+            'id_matpel' => 24
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Maman Somawijaya,SST.',
+            'id_matpel' => 24
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Solihing Dzuldin,S.Pd.',
+            'id_matpel' => 25
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Lutfi Zainufrrihan,S.Pd.',
+            'id_matpel' => 25
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Hj.Laswati,SST.',
+            'id_matpel' => 25
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Lutfi Zainufrrihan,S.Pd.',
+            'id_matpel' => 26
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Sigit Dwi Prajoso,S.Pd.',
+            'id_matpel' => 26
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dadan Hermawan,S.Pd.',
+            'id_matpel' => 27
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Suwagiyono,S.Pd.',
+            'id_matpel' => 28
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Usi Nur Azizah,S.Pd.',
+            'id_matpel' => 28
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Moh.Taufan,S.Pd.',
+            'id_matpel' => 29
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dra.Hj.Putri Elfia',
+            'id_matpel' => 29
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Lilis Hamidah,S.Pd.',
+            'id_matpel' => 30
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Erwin Nugraha,S.Pd.',
+            'id_matpel' => 30
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Lilis Hamidah,S.Pd.',
+            'id_matpel' => 31
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Yudi Kartiwa,SST,S,Pd.',
+            'id_matpel' => 31
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Paojan Muhamad Akbar,S.Pd.',
+            'id_matpel' => 32
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nisrina Athirah Emelda,S.Pd.',
+            'id_matpel' => 32
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Yofi Fikri Islami,S.Pd.',
+            'id_matpel' => 32
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dudin Wahyudin,SE.',
+            'id_matpel' => 33
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Yofi Fikri Islami,S.Pd.',
+            'id_matpel' => 33
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Cuncun Cahyadi,S.Si.M.M',
+            'id_matpel' => 34
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nono Suwarno,S.Kom.',
+            'id_matpel' => 34
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Taopik Sidqi,M.Pd.',
+            'id_matpel' => 35
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Aditya Ramadhan,S.Pd.',
+            'id_matpel' => 35
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Moh. Gilang Ramadhan,S.Pd.',
+            'id_matpel' => 35
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Iwan Gunawan',
+            'id_matpel' => 36
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Aditya Ramadhan,S.Pd.',
+            'id_matpel' => 36
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Tyas Sawiji,S.Pd.',
+            'id_matpel' => 36
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Cuncun Cahyadi,S.Si,M.M.',
+            'id_matpel' => 37
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Aditya Ramadhan,S.Pd.',
+            'id_matpel' => 37
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nono Suwarsono,S.Kom.',
+            'id_matpel' => 37
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rani Ratnaningsih,S.Pd.',
+            'id_matpel' => 38
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Yuyun Tresnawati,M.Kom.',
+            'id_matpel' => 38
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Moh Ali Aljauhari,S.Kom.',
+            'id_matpel' => 39
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rina Pujiati,STT.',
+            'id_matpel' => 39
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Lukmanul Hakim Firdaus,S.Kom.',
+            'id_matpel' => 40
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Offy Solehatun,S.I.Kom,M.Kom.',
+            'id_matpel' => 40
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rina Pujiati,STT.',
+            'id_matpel' => 40
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Ferry Stephanus,S.Kom.',
+            'id_matpel' => 41
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Hana Zainab Mukarromah,S.T.',
+            'id_matpel' => 41
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Bahar Nugraha Praja,S.Pd.',
+            'id_matpel' => 42
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Ivan Irianto,SS.',
+            'id_matpel' => 42
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Rida Mulyadi,S.Pd.',
+            'id_matpel' => 43
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Selfa Amalia Rahman,S.Pd.',
+            'id_matpel' => 43
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Arif Syarifudin,S.T.',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Dadi Herdiansyah',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Neneng Netti S,S.Pd.',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Amalia Utami,S.Pd.',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Iman Firmansyah,S.Pd.',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Iwan Gunawan',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Shinta Pusparani,S.Pd.',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Nisrina Athirah Emelda,S.Pd.',
+            'id_matpel' => 44
+        ]);
+        DB::table('guru')->insert([
+            'nama_guru' => 'Drs.Rizal Sapari',
+            'id_matpel' => 44
+        ]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
